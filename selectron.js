a3a43964cdcc4f66201b4b0d06617e5d777842c3
yonkeltron <yonkeltron@gmail.com>
2010-08-01 15:03:25 -0400

    wrote out needless iteration (thanks to _.breakLoop(), which might be ugly?),  and got property matching working
    Signed-off-by: yonkeltron <yonkeltron@gmail.com>

--- selectron.js
+++ selectron.js
@@ -1,18 +1,39 @@
 var selectron = {};
 selectron.util = {};
 
-selectron.util.has_keys = function has_keys(obj, keylist) {
-    var obj_keys, matches;
-    obj_keys = _.keys(obj);
+selectron.util.has_keys = function has_keys(obj, temp) {
+    var obj_keys, temp_keys, matches;
+    // get the keys of the target object
+    obj_keys = _(obj).keys();
+    // get the keys of the template
+    temp_keys = _(temp).keys();
     matches = true;
-    _(keylist).each( function (key) {
-        if ( matches && !(_(obj_keys).include(key)) ) {
+
+    _(temp_keys).each( function (key) {
+        // if the key is not present in the target object, set false and break
+        if ( !(_(obj_keys).include(key)) ) {
             matches = false;
+            _.breakLoop();
         }
     });
     return matches;
 };
 
 selectron.util.object_match = function object_match(object, template) {
-    
+    var matches = true;
+
+    if ( selectron.util.has_keys(object, template) ) { // first, see if the object has all the right keys
+        // go over all of the temlate properties
+        _(template).chain()
+        .keys()
+        .each( function (key) {
+            if ( !(template[key] === object[key]) ) { // if the properties aren't equal, set false and break
+                matches = false;
+                _.breakLoop();
+            }
+        });
+    } else {
+        matches = false;
+    }
+    return matches;
 };
--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
+var test_obj = {
+    panda: 1,
+    bamboo: 2,
+    curry: 3
+};
+
 module("Basic structure testing");
 test("selectron namespace", function () {
+    ok(_, "underscore object exists");
     ok(selectron, "selectron exists");
     ok(selectron.util, "selectron.util exists");
 });
@@ -8,19 +15,31 @@ test("selectron namespace", function () {
 module("Object matching");
 
 test("selectron.object_match", function () {
-    ok(selectron.util.object_match, "selectron.util.object_match exists");
-    ok(_.isFunction(selectron.util.object_match), "selectron.util.object_match is a function");
+    ok(selectron.util.object_match, 
+       "selectron.util.object_match exists");
+
+    ok(_.isFunction(selectron.util.object_match), 
+       "selectron.util.object_match is a function");
+
+    ok(selectron.util.object_match(test_obj, {panda: 1}),
+       "selectron.util.object_match returns true for matching template object with one key");
+
+    ok(selectron.util.object_match(test_obj, {panda: 1, bamboo: 2}),
+       "selectron.util.object_match returns true for matching template object with two keys");
+
+    ok(!selectron.util.object_match(test_obj, {panda: 1, bamboo: 3}),
+       "selectron.util.object_match returns false for non-matching template object with two present keys");
+
+    ok(!selectron.util.object_match(test_obj, {panda: 1, chicken: 2}),
+       "selectron.util.object_match returns false for non-matching template object with mixed-presence keys");
 });
 
-test("matching based on keys", function () {
-    var test_obj = {
-        panda: 1,
-        bamboo: 2,
-        curry: 3
-    };
-
+test("key checking with selectron.util.has_keys", function () {
     ok(selectron.util.has_keys, "selectron.util.has_keys exists");
     ok(_.isFunction(selectron.util.has_keys), "selectron.util.has_keys is a function");
-    ok(selectron.util.has_keys(test_obj, ["panda", "bamboo"]),
-       "object_match passes given correct keys");
+    ok(selectron.util.has_keys(test_obj, {panda: 'whatever', bamboo: 'whatever'}),
+       "has_keys passes given correct keys");
+    ok(!selectron.util.has_keys(test_obj, {panda: 'whatever', chocolate: 'missing'}),
+       "has_keys fails given incorrect keys");
+    
 });
